14e248b4f32a76afa28faff3c8446b175192251a
Nicolas Verdonck <nverdonc@student.42.fr>
2016-03-17 15:21:43 +0100

    add get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nverdonc <nverdonc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/10 13:15:11 by nverdonc          #+#    #+#             */
+/*   Updated: 2015/01/26 14:41:42 by nverdonc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static unsigned int		ft_next_n(char *str)
+{
+	unsigned int		i;
+
+	i = 0;
+	if (str)
+	{
+		while (str[i] && str[i] != '\n')
+			i++;
+	}
+	return (i);
+}
+
+int			get_next_line(int const fd, char **line)
+{
+	char			buf[BUFF_SIZE + 1];
+	static	char	*end = NULL;
+	char			*tmp;
+	char			*tmp_free;
+	int				ret;
+
+	*line = NULL;
+	tmp = end;
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[ret] = '\0';
+		tmp_free = tmp;
+		tmp = ft_strjoin(tmp, buf);
+		if (tmp_free)
+			ft_strdel(&tmp_free);
+		if (ft_next_n(buf) != ft_strlen(buf))
+			break ;
+	}
+	*line = ft_strsub(tmp, 0, ft_next_n(tmp));
+	end = ft_strsub(tmp, ft_next_n(tmp) + 1, ft_strlen(tmp));
+	ft_strdel(&tmp);
+	if ((*line && **line) || *buf == '\n')
+		return (1);
+	return (0);
+}
